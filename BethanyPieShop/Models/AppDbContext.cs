f7c16426551626dd2f44a9eef739ab2f991d02e3
lxb768 <lalita.bhetwal@sherwin.com>
2017-12-08 15:48:43 -0500

    Initialized db context with ef

--- BethanyPieShop/Models/AppDbContext.cs
+++ BethanyPieShop/Models/AppDbContext.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace BethanyPieShop.Models
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Pie> Pies { get; set; }
     }
-}
+} 
--- BethanyPieShop/Startup.cs
+++ BethanyPieShop/Startup.cs
@@ -6,18 +6,31 @@ using BethanyPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace BethanyPieShop
 {
     public class Startup
     {
+        private IConfigurationRoot _configurationRoot;
+        public Startup(IHostingEnvironment hostingEnvironment)
+        {
+            _configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath).
+                AddJsonFile($"appsettings.{hostingEnvironment.EnvironmentName}.json",true).
+                Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
-            services.AddTransient<IPieRepository, MockPieRepository>();
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection")));
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IPieRepository, PieRepository>();
             services.AddMvc();
         }
 
@@ -28,9 +41,13 @@ namespace BethanyPieShop
             {
                 app.UseDeveloperExceptionPage();
                 app.UseStatusCodePages();
-                app.UseStaticFiles();
-                app.UseMvcWithDefaultRoute();
             }
+            else
+            {
+                app.UseExceptionHandler("/AppException");
+            }
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
