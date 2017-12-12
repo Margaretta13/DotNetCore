e617e7838a437e467cc6d2d6d04a97da6e7f2b27
lxb768 <lalita.bhetwal@sherwin.com>
2017-12-12 13:50:11 -0500

    ShoppingCart model creation in progress

---
+++ BethanyPieShop/Models/ShoppingCart.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BethanyPieShop.Models
+{
+    public class ShoppingCart
+    {
+        private AppDbContext _appDbContext;
+
+        private ShoppingCart(AppDbContext context)
+        {
+            _appDbContext = context;
+        }
+
+        public string ShoppingCartId { get; set; }
+
+        public IEnumerable<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public static ShoppingCart GetCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+            string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+            session.SetString("CartId", cartId);
+
+            var context = services.GetService<AppDbContext>();
+            return new ShoppingCart(context) { ShoppingCartId = cartId };
+
+            //return new ShoppingCart(_appDbContext) {ShoppingCartId = cartId};
+        }
+
+        public void AddToCart(Pie pie, int amount)
+        {
+            
+        }
+    }
+}
