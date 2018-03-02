4f973a32f94148c9c31494d3e7a9d4cc24716094
lxb768 <lalita.bhetwal@sherwin.com>
2018-03-02 11:36:21 -0500

    wip

---
+++ BethanyPieShop/Controllers/ShoppingCartController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanyPieShop.Models;
+using BethanyPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanyPieShop.Controllers
+{
+    public class ShoppingCartController
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartController(IPieRepository pieRepository, ShoppingCart shoppingCart)
+        {
+            _pieRepository = pieRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public ViewResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(shoppingCartViewModel);
+        }
+    }
+}
